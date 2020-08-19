16074b9648f1c7577d703b0e71cc46146d63cf68
unknown <corbettjustis@gmail.com>
2020-08-18 19:02:40 -0600

    created models and added app to settings

--- drummachine/models.py
+++ drummachine/models.py
@@ -1,3 +1,19 @@
+import os
+from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def sounds_path():
+    return os.path.join(settings.STATIC_URL, 'sounds')
+
+
+class Rack(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="racks")
+    instruments = models.JSONField()
+
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=25)
+    path = models.FilePathField(path=sounds_path, allow_files=True)
+    
