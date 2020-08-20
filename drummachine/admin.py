714f83fabb23d305161075ef830ad04306b194cf
unknown <corbettjustis@gmail.com>
2020-08-20 16:06:10 -0600

    added kit model to admin panel

--- drummachine/admin.py
+++ drummachine/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Rack, Instrument
+from .models import Rack, Kit, Instrument
 # Register your models here.
 admin.site.register(Rack)
 admin.site.register(Kit)
