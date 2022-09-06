c5cf56beb0425ffacc8aacdd4f83835647d1356e
JustisCorbett <corbettjustis@gmail.com>
2022-09-06 23:42:57 +0000

    reconfigure settings for self serving

--- beatbits/settings.py
+++ beatbits/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from configparser import RawConfigParser
+
+config = RawConfigParser()
+config.read('/web/settings.ini')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = config.get('secretkey', 'secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.212', 'localhost']
 
 
 # Application definition
@@ -75,10 +79,15 @@ WSGI_APPLICATION = 'beatbits.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        os.environ["DATABASE_URL"]
-    }
+DATABASES = {  
+	'default': {     
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',       
+		'NAME': 'beatbits',       
+		'USER': 'beatbitsuser',        
+		'PASSWORD': config.get('pgpassword','password'),        
+		'HOST': 'localhost',       
+		'PORT': '',    
+	}
 }
 
 
@@ -130,5 +139,5 @@ LOGIN_URL = '/login'
 
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
