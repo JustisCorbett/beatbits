3cce4443a1270f8249dbb2029470dfff4cdfd6fd
JustisCorbett <corbettjustis@gmail.com>
2023-01-16 23:55:10 +0000

    turn off debug mode

--- beatbits/settings.py
+++ beatbits/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.get('secretkey', 'secretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['beatbits.justiscorbett.com','10.0.0.212', 'localhost']
 
